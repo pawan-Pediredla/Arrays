a0cc121ddca9e7046c3b3c503fe1cb69c7a824bd
pawan-Pediredla <150579399+pawan-Pediredla@users.noreply.github.com>
2024-07-01 15:35:53 +0530

    Update index.js

--- Remove-elements/index.js
+++ Remove-elements/index.js
@@ -2,3 +2,8 @@
 console.log(fruits)
 console.log(fruits.pop())
 console.log(fruits.shift())
+let fruitss=['orange','apple']
+let newConstrc=new Array(1,2,3,4)
+newConstrc.push(4)
+newConstrc.shift()
+newConstrc.unshift("hi")
